54cf7fda99c131b6ee05b1ecc8e273f6f4ad5fbe
andersonlimaz <andersonlimaz@outlook.com>
2023-09-01 09:17:54 -0300

    aula 5 commit

--- aula5.js
+++ aula5.js
@@ -1,13 +1,8 @@
 // else if, operadores de condicional simples. 
 const numero = 0;
 
-const numeroDivisivelPor5 = (numero % 5 ) === 0;
-
-if (numero === 0){
-    console.log('O número é invalido');
-}else if (numeroDivisivelPor5) {
-    console.log('Sim');
-
-} else{
-  console.log('Não');
+if ((numero % 2 )===0 ){
+  console.log('O numero é par');
+} else {
+  console.log('O numero é impar');
 }
