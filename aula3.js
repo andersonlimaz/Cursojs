2b0d8049ec43f98c65fa6d5a44325665aecedf72
andersonlimaz <andersonlimaz@outlook.com>
2023-09-01 08:57:58 -0300

    firstt commit js

---
+++ aula3.js
@@ -0,0 +1,9 @@
+// = -> atribuição 
+// == -> igualdade ignorando o tipo da variavel, fazendo uma conversão implicita 
+// === -> igualdade ele diferencia (string, num, Boolean)
+
+const numero = 2;
+
+const numeroPar = (numero % 2 ) === 0;
+
+console.log(numeroPar)
---
+++ aula4.js
@@ -0,0 +1,43 @@
+// // if else
+// const numero = 2;
+
+// const numeroPar = (numero % 2 ) === 0;
+
+
+// if (numeroPar) {
+//     console.log('Par')
+
+// }
+// // ! -> como se fosse not
+// if (!numeroPar){
+//     console.log('Impar')
+
+// }
+// if else
+//--------------------------------------------------------------------------------------------
+// const numero = 2;
+
+// const numeroPar = (numero % 2 ) === 0;
+
+
+// if (numeroPar) {
+//     console.log('Par');
+
+// } else{
+//   console.log('Impar');
+
+// }
+//--------------------------------------------------------------------------------------------
+
+const numero = 2;
+
+const numeroDivisivelPor5 = (numero % 5 ) === 0;
+
+
+if (numeroDivisivelPor5) {
+    console.log('Sim');
+
+} else{
+  console.log('Não');
+
+}
