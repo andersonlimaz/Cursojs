a3002efe484dad25b23d98ff3dcd84421e1eca81
andersonlimaz <93233117+andersonlimaz@users.noreply.github.com>
2023-09-02 13:28:13 -0300

    commit ultimo de sabado

---
+++ 04-ConceitosBasicosFunções/teste.js
@@ -0,0 +1,12 @@
+// function calcularMedia(nota1, nota2) {
+//     const media = (nota1 + nota2) / 2;
+// }
+
+// console.log(calcularMedia(5, 5));x
+
+function incrementarJuros(valor, percentualDeJuros) {
+    const incremento = (percentualDeJuros / 100) * valor;
+    return valor + incremento;
+}
+
+incrementarJuros(100,10)+ 100
