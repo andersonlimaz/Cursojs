4eb23cf24af997d91db987302a04a8a6d72bb8b8
andersonlimaz <93233117+andersonlimaz@users.noreply.github.com>
2023-09-03 12:15:40 -0300

    commit aula1

---
+++ 05-Objetos/aula1.js
@@ -0,0 +1,15 @@
+/* Objetos
+podemos atribuir um objeto, forma mais simples é objeto literal, 
+objeto é uma coleção de valores, agrupa valores, funciona atraves de chave e valor. 
+
+obj literal ex: 
+*/
+
+const victor = {
+    nome: 'victor J Guerra', 
+    idade: 25
+};
+
+console.log(victor.nome);
+console.log(victor.idade);
+console.log(victor);
