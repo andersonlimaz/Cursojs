da5303c696523324aa14dad8bf9442514643115a
andersonlimaz <andersonlimaz@outlook.com>
2023-09-04 12:47:08 -0300

    commit primeiros exemplos arrays

---
+++ 08-Arrays/aula1.js
@@ -0,0 +1,21 @@
+
+// podem ser adicionado lista e numeros. 
+// const alunos = ['joao','vitor', 'marina'];
+// alunos.push('Anderson') // adiciona Anderson na lista.
+// alunos[2] = 'Vinicius'; // adiciona na posição que você quiser, sobrescreve o anterior. 
+// console.log(alunos);
+// // -----------------------------------------ex1
+
+// const alunos = ['1','2','3'] //Podemos iniciar a lista vazia
+
+// alunos.push('teste');
+// alunos.push(10);// podemos adicionar num e strings. 
+// alunos.pop(); // remove o item mais proximo
+// console.log(alunos);
+// -------------------------------------------------------ex2
+const alunos = ['1','2','3'] 
+
+console.log(alunos);
+// console.log(alunos.pop()); // tira o ultimo resultado e traz qual foi obtido. 
+console.log(alunos.shift()); // tira o primeiro resultado e traz qual foi obtido. 
+console.log(alunos);
