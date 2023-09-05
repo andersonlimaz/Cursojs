76223afcb815ce55983483939c1daf40a73bfdd9
andersonlimaz <andersonlimaz@outlook.com>
2023-09-05 11:35:53 -0300

    commit exercicio resolvido

--- 09-importaçãoExportaçãoJs/main.js
+++ 09-importaçãoExportaçãoJs/main.js
@@ -1,7 +1,34 @@
 /*
 como organizar os codigo e importação exportação js 
 */
+/*
+Uma sala contem 5 alunos e para cada aluno foi sorteado um número de 1 - 100.
+Faça um programa que recebe os 5 números sorteados para os alunos e mostre o maior números sorteado. 
+Dados de entrada:
+5
+50
+10
+98
+23
+
+
+saída:
+98
+*/ 
 const {gets, print} = require('./funcoes-auxiliares.js');
 
+const numerosSorteados = [];
 
-print(gets())
+for (let i = 0; i < 5; i++) {
+    const numerosSorteado = gets();
+    numerosSorteados.push(numerosSorteado);
+}
+
+let maiorValor= 0;
+for (let i = 0; i < numerosSorteados.length; i++){
+    const numerosSorteado = numerosSorteados[i];
+    if (numerosSorteado > maiorValor){
+        maiorValor = numerosSorteado;
+    }
+}
+print(maiorValor)
