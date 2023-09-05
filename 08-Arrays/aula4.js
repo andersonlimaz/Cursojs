c1697c6605eeaf26b670c4768a0b3b1b623374e1
andersonlimaz <andersonlimaz@outlook.com>
2023-09-05 08:54:54 -0300

    commit primeiro exercicio do dia

--- 08-Arrays/aula4.js
+++ 08-Arrays/aula4.js
@@ -3,7 +3,10 @@ primeira parte- declaração do index, ela indica o inicio
 segunda parte- estrutura condicional, que avalia se entra ou não dentro da proxima condição. 
 terceira parte- O que vai acontecer depois de terminar de executar o for. no exemplo o i++ incrementa o 1 na variavel i. 
 */
+const nome= 'Anderson brito de lima'
 
-for (let i = 0; i < 10; i++) { 
-    console.log(i);  
+
+
+for (let i = 0; i < nome.length; i++) { 
+    console.log(nome[i]);
 }
