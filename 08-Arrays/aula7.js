67b1e0d073ad48d1b85e56132fbc2ed54b7ab9be
andersonlimaz <andersonlimaz@outlook.com>
2023-09-05 10:28:32 -0300

    commit exercicio da tabuada

---
+++ 08-Arrays/aula7.js
@@ -0,0 +1,9 @@
+/* 
+1) Crie um programa que dado o um número imprima a sua tabuada. 
+*/
+
+const numero = 7
+for (let i = 1; i <= 10; i++) {
+    console.log(i + ' X ',numero + ' = '+ i * numero);
+    
+}
