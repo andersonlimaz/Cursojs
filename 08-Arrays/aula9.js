ea4b9809d22c458d4fbc7c56f0e8bacd507d0512
andersonlimaz <andersonlimaz@outlook.com>
2023-09-05 10:58:35 -0300

    exercicios do teste

---
+++ 08-Arrays/aula9.js
@@ -0,0 +1,28 @@
+// const numeros = [];
+
+// for (let i = 0; i < 10; i++) {
+//     numeros.push(i);
+// }
+
+// console.log(numeros);
+
+// const numerosPares = [];
+
+// for (let i = 0; i < 10; i = i + 2) {
+//     numerosPares.push(i);
+// }
+
+// console.log(numerosPares);
+// const numerosPares = [];
+
+//     for (let i = 0; i < 10; i++) {
+//         const numeroPar = i % 2 === 0;
+//         if (numeroPar) {
+//             numerosPares.push(i);
+//         }
+//     }
+
+//     console.log(numerosPares);
+const listaDeValores = [1, 2, 3, 4, 5];
+const valor = listaDeValores[2];
+console.log(valor);
